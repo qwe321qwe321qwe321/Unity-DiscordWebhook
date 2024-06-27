7bd4c2f000af1553dd0b70145e19b28ea00b265f
qwe321qwe321qwe321 <aegis321123@gmail.com>
2024-06-27 13:50:05 +0800

    RefreshRateRatio not available before Unity 2022

--- src/DiscordWebhook/Assets/DiscordWebhook/SystemInfoHelper.cs
+++ src/DiscordWebhook/Assets/DiscordWebhook/SystemInfoHelper.cs
@@ -10,10 +10,19 @@ namespace DiscordWebhook {
                 $"* GPU Memory Size: {SystemInfo.graphicsMemorySize} \n" +
                 $"* Graphics API: {SystemInfo.graphicsDeviceType}\n" +
                 $"* OS: {SystemInfo.operatingSystem} \n" +
-                $"* Screen: {Screen.width}x{Screen.height} @{Screen.currentResolution.refreshRateRatio}Hz, fullScreenMode={Screen.fullScreenMode}, vSync={QualitySettings.vSyncCount}, targetFrameRate={Application.targetFrameRate}\n" +
+                
+                $"* Screen: {Screen.width}x{Screen.height} @{GetMonitorRefreshRateHz()}, fullScreenMode={Screen.fullScreenMode}, vSync={QualitySettings.vSyncCount}, targetFrameRate={Application.targetFrameRate}\n" +
                 $"* buildGUID: {Application.buildGUID}, isEditor={Application.isEditor}\n" +
                 $"* Unity: {Application.unityVersion}\n"
                 ;
+        }
+
+        public static string GetMonitorRefreshRateHz() {
+#if UNITY_2022_2_OR_NEWER
+            return Screen.currentResolution.refreshRateRatio.ToString() + "Hz";
+#else
+            return Screen.currentResolution.refreshRate.ToString() + "Hz";
+#endif
         }
     }
 }
